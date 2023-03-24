a10925e59df9bd3ea559d53e2eac44320d3e4237
louie_gomez <kinggomez027.lg@gmail.com>
2023-03-24 17:46:59 -0400

    generate review resource & add all relation macros

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,3 +1,5 @@
 class Trip < ApplicationRecord
     belongs_to :user
+    has_many :reviews
+    has_many :users, through: :reviews
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
+
     has_many :trips
+    has_many :reviews
+    has_many :trips, through: :reviews
 end
