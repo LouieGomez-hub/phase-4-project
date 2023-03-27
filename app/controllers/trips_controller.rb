1711144164384f8dba7a080429d32b7b9edbd711
louie_gomez <kinggomez027.lg@gmail.com>
2023-03-27 18:51:10 -0400

    3/4 crud actions created for trip controller w/ error messaging

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,2 +1,30 @@
 class TripsController < ApplicationController
+
+    def index
+        trip = Trip.all
+        render json: trip
+    end
+
+    def show
+        trip = Trip.find_by(id: params[:id])
+        render json: trip, status: :created
+    end
+
+    def create
+        trip = Trip.create(trip_params)
+        if trip.valid?
+            session[:trip_id] = trip.id
+            render json: trip, status: :created
+        else
+            render json: { error: "Not authorized" }, status: :unauthorized unless session.include? :trip_id
+        else
+            render json: { error: user.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def trip_params
+        params.permit(:name, :location, :price, :rating, :description, :user_id)
+    end
 end
