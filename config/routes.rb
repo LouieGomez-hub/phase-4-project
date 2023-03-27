74408a25f02544e43fdb1e130a69c15c41bb4d18
louie_gomez <kinggomez027.lg@gmail.com>
2023-03-27 14:45:53 -0400

    updated routes for controller actions

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   resources :reviews
   resources :trips
   resources :users
+  post "/signup", to: "users#create"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   get '*path',
       to: 'fallback#index',
       constraints: ->(req) { !req.xhr? && req.format.html? }
