c137eaf4da7460e3734e6053f5067da2c45014ba
louie_gomez <kinggomez027.lg@gmail.com>
2023-03-27 15:00:12 -0400

    updated routes for controller actions

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :trips
   resources :users
   post "/signup", to: "users#create"
+  get "/me", to: "users#show"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
   get '*path',
