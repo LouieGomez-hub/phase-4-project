a4beed8a772a960c6146e9be1661fa8407e84b99
louie_gomez <kinggomez027.lg@gmail.com>
2023-03-27 14:43:27 -0400

    create action with authorization & error messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,23 @@
 class UsersController < ApplicationController
+    before_action :authorize, only: [:show]
+
+    def create
+        user = User.create(user_params)
+        if user.valid?
+            session[:user_id] = user.id
+            render json: user, status: :created
+        else
+            render json: { error: user.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def user_params
+        params.permit(:username, :password, :password_confirmation)
+    end
+
+    def authorize
+        return render json: { error: "Not authorized" }, status: :unauthorized unless session.include? :user_id
+    end
 end
