b6f84df0f09f9d9921e184ced20398b6b72c32f5
louie_gomez <kinggomez027.lg@gmail.com>
2023-03-27 17:40:55 -0400

    removed users resource from route

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   resources :reviews
   resources :trips
-  resources :users
+
   post "/signup", to: "users#create"
   get "/me", to: "users#show"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
+  
   get '*path',
       to: 'fallback#index',
       constraints: ->(req) { !req.xhr? && req.format.html? }
