de10b6871a50dae824e74f9663a28f4032464ca3
louie_gomez <kinggomez027.lg@gmail.com>
2023-03-31 11:28:10 -0400

    adjusted code for version 18

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+root.render(
   <React.StrictMode>
     <Router>
       <App />
     </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
+
+reportWebVitals();
