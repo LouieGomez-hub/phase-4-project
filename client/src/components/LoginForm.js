c20aba9c9ce00ff9ebaae7b12d6a8925edb84c32
louie_gomez <kinggomez027.lg@gmail.com>
2023-04-03 13:28:54 -0400

    added code for login functionality

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,20 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 
-function LoginForm() {
+function LoginForm({ onLogin }) {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+
+    function handleLogin(e) {
+        e.preventDefault();
+
+        fetch("/login", {
+            method: 'POST',
+            headers: { 
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ username, password })
+        })
+        .then((res) => {
+            if(res.ok) {
+                res.json().then((user) => onLogin(user))
+            } else {
+                res.json().then((err) => setError(`${Object.keys(err)}: ${Object.values(err)}`))
+            }
+        })
+    }
 
     return (
-        <div></div>
+        <div>
+            <form className="myacc" onSubmit={handleLogin}>
+                <input
+                    type="text"
+                    value={username}
+                    placeholder="Username"
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+                <input
+                    type="password"
+                    value={password}
+                    placeholder="Password"
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                {error ? <div className="errorContainer">
+                    <span className="error-message">{error}</span>
+                </div> : null
+                }
+                <button variant="info" type="submit">Login</button>
+            </form>
+        </div>
     );
 }
-
-
-
-
-
-
-
-
-
-
 
 export default LoginForm;
