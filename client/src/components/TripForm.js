42adc72aa8cd6cec26cc0b91d88876446cc6e9c3
louie_gomez <kinggomez027.lg@gmail.com>
2023-04-03 13:29:29 -0400

    added code to book your own trip

--- client/src/components/TripForm.js
+++ client/src/components/TripForm.js
@@ -1,20 +1,102 @@
-import React from "react";
+import React, { useState } from "react";
 
-function TripForm() {
+function TripForm({ onAddTrip }) {
+    const [errors, setErrors] = useState([]);
+    const [trips, setTrips] = useState({
+        name: "",
+        location: "",
+        description: "",
+        price: "",
+        rating: 0
+    })
+    
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        setErrors([]);
+
+        fetch("/trips", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(trips)
+        })
+        .then((res) => {
+            if(res.ok) {
+                onAddTrip(trips)
+                setTrips({name: "", location: "", description: "", price: "",})
+            } else {
+                res.json().then((err) => setErrors(err.errors));
+            }
+        })
+    }
+
+    function handleChange(e) {
+        setTrips({...trips, 
+            [e.target.name]: e.target.value
+        })
+    }
 
     return (
-        <div></div>
+        <div>
+            <form className="NewTrip" onSubmit={handleSubmit}>
+                <h4>Book Your Own Trip</h4>
+                <label>Hotel
+                    <br/>
+                    <input 
+                        type="text"
+                        name="name"
+                        value={trips.name}
+                        placeholder="Hotel"
+                        onChange={(e) => handleChange(e)}
+                    />  
+                </label>
+
+                <label>Location
+                    <br/>
+                    <input 
+                        type="text"
+                        name="location"
+                        value={trips.location}
+                        placeholder="Italy"
+                        onChange={(e) => handleChange(e)}
+                    />  
+                </label>
+
+                <label>Price
+                    <br/>
+                    <input 
+                        type="number"
+                        name="price"
+                        value={trips.price}
+                        placeholder="100"
+                        onChange={(e) => handleChange(e)}
+                    />  
+                </label>
+
+                <label htmlFor="description">Description
+                    <br/>
+                    <textarea 
+                        type="textarea"
+                        name="description"
+                        value={trips.description}
+                        placeholder="Tell us more..."
+                        onChange={(e) => handleChange(e)}
+                    >
+                    </textarea>      
+                </label>
+
+                <div className="errorContainer">
+                    {errors.map((err) => (
+                        <span className="error-message" key={err}>{`Invalid: ${err}`}</span>
+                    ))
+                    }
+                </div>
+                <button variant="warning" type="submit">Submit</button>
+            </form>
+        </div>
     );
 }
-
-
-
-
-
-
-
-
-
-
 
 export default TripForm;
