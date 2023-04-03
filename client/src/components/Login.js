ed3730afdeae02d47cee819eaaf3e2f670c8fa30
louie_gomez <kinggomez027.lg@gmail.com>
2023-04-03 13:28:19 -0400

    created login/signup pop up screen

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,20 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import LoginForm from "./LoginForm";
+import Signup from "./Signup";
 
-function Login() {
+function Login({ onLogin }) {
+    const [showLogin, setShowLogin] = useState(true);
 
     return (
-        <div></div>
+        <div className="login-card">
+            <h1>
+                { showLogin ? "Welcome back" : "Create an account" }
+            </h1>
+            { showLogin ? <LoginForm onLogin={onLogin}/> : <Signup onLogin={onLogin}/> }
+            <button onClick={() =>  setShowLogin(!showLogin)}>
+                { showLogin ? "or sign up" : "or log in" }
+            </button>
+        </div>
     );
 }
-
-
-
-
-
-
-
-
-
-
 
 export default Login;
