652f47fe726fc470bec040e934ec6d621f3aeb9a
louie_gomez <kinggomez027.lg@gmail.com>
2023-04-03 13:30:04 -0400

    code for specific user account pgae

--- client/src/components/User.js
+++ client/src/components/User.js
@@ -1,20 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import UserReviews from "./UserReviews";
+import UserTrips from "./UserTrips";
+import TripForm from "./TripForm";
 
 function User() {
+    const [reviews, setReviews] = useState([]);
+    const [trips, setTrips] = useState([]);
+
+    useEffect(() => {
+        fetch("/me")
+          .then((res) => {
+            if(res.ok) {
+              res.json().then((user) => {
+                setTrips(user.trips)
+                setReviews(user.reviews)
+              });
+            }
+        });
+      }, []);
+    
+    function handleAddTrip(newTrip) {
+        setTrips([...trips, newTrip])
+    }
 
     return (
-        <div></div>
+        <div className="user-container">
+            <TripForm onAddTrip={handleAddTrip} />
+            <UserTrips trips={trips} />
+            <UserReviews reviews={reviews} />
+        </div>
     );
 }
-
-
-
-
-
-
-
-
-
-
 
 export default User;
