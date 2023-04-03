d856f4d80454d17d54ceee999cf49d38d20d3b00
louie_gomez <kinggomez027.lg@gmail.com>
2023-04-03 14:36:40 -0400

    created and styled navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,20 +1,42 @@
 import React from "react";
 
-function NavBar() {
+function NavBar({ user, setUser }) {
+
+    function handleLogout() {
+        fetch("/logout", {
+            method: 'DELETE',
+        })
+        .then((res) => {
+            if(res.ok) {
+                setUser(null);
+            }
+        })
+        .then(<redirect to="/" />)
+    }
+
+    function handleNavButtons() {
+        if(user) {
+            return (
+                <div>
+                    <NavBar.Text className="current-user">
+                        <b>{`@${user.username}`}</b>
+                    </NavBar.Text>
+                    <button variant="info" href="/account">My Account</button>
+                    <button variant="outline-info" onClick={handleLogout}>Logout</button>
+                </div>
+            );
+        }
+    }
 
     return (
-        <div></div>
+        <div>
+            <NavBar>
+                <NavBar.Collapse className="justify-content-end">
+                    {handleNavButtons}
+                </NavBar.Collapse>
+            </NavBar>
+        </div>
     );
 }
-
-
-
-
-
-
-
-
-
-
 
 export default NavBar;
