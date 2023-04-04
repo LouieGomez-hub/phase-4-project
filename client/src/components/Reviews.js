e8ce91d19b04404e6cfa2256fdfbf721aa1fb86f
louie_gomez <kinggomez027.lg@gmail.com>
2023-04-04 16:16:31 -0400

    created buttons for edit and delete functionality

--- client/src/components/Reviews.js
+++ client/src/components/Reviews.js
@@ -1,20 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Reviews() {
+function Reviews({ review, user, errors, onUpdateReview, onDeleteReview }) {
+    const [edit, setEdit] = useState(false);
+    const [comment, setComment] = useState(review.comment);
+
+    function editReview() {
+        setEdit(!edit)
+        onUpdateReview(review.id, comment)
+    }
 
     return (
-        <div></div>
+        <div className="review">
+            {user.id === review.user.id ? 
+                <div className="reviewBtn">
+                    {!edit ?
+                        <button variant="outline-info" onClick={() => {
+                            setEdit(!edit)
+                        }}
+                        >Edit</button>
+                    :
+                        <button variant="warning" onClick={editReview}>Save Edits</button>
+                    }
+
+                    {!edit && (
+                        <button onClick={() => {onDeleteReview(review.id)}}>
+                            X
+                        </button>
+                    )}
+                </div> : null
+            }
+            <div>
+                {(edit && review.user.id === user.id) ? 
+                    <form className="reviewEdits">
+                        <label htmlFor="comment"></label>
+                        <input
+                            type="textarea"
+                            rows="rand(3..5)"
+                            name="comment"
+                            value={comment}
+                            onChange={(e) => setComment(e.target.value)}
+                        />
+                    </form>
+                    : <p>{review.comment}</p>
+                }
+                <span className="reviewUser">
+                    <b>{`@${review.user.username}`}</b>
+                </span>
+            </div>
+        </div>
     );
 }
-
-
-
-
-
-
-
-
-
-
 
 export default Reviews;
