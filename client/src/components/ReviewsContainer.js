fde76bbf7dc6c9d40992336c87dc6eea12a87e6e
louie_gomez <kinggomez027.lg@gmail.com>
2023-04-04 15:54:17 -0400

    holding code for updating and deleting reviews

--- client/src/components/ReviewsContainer.js
+++ client/src/components/ReviewsContainer.js
@@ -1,20 +1,80 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import Reviews from "./Reviews";
+import ReviewForm from "./ReviewForm";
 
-function ReviewsContainer() {
+function ReviewsContainer({ user }) {
+    const params = useParams();
+    const [reviews, setReviews] = useState([]);
+
+    useEffect(() => {
+        fetch(`/trips/${params.id}/reviews`)
+        .then((res) => res.json())
+        .then(setReviews);
+    }, [params.id])
+
+    function updateReviews(reviewData) {
+        setReviews([...reviews, reviewData])
+    }
+
+    function handleUpdateReview(reviewID, comment) {
+        fetch(`/reviews/${reviewID}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ comment: comment })
+        })
+        .then(res => {
+            if(!res.ok) {
+                res.json().then((err) => {
+                    alert(err.errors)
+                    return reviews
+                })
+            } else {
+                setReviews((reviews) => {
+                    let updatedReviews = reviews.map(review => {
+                        if(review.id === reviewID) {
+                            review.comment = comment
+                        }
+                        return review
+                    })
+                    return updatedReviews
+                })
+            }
+        })
+
+    }
+
+    function handleDeleteReview(reviewID) {
+        fetch(`/reviews/${reviewID}`, {
+            method: 'DELETE',
+        })
+        .then(res => {
+            if (res.ok) {
+                setReviews(reviews.filter((review) => {
+                    return review.id !== reviewID
+                }))
+            }
+        })
+    }
 
     return (
-        <div></div>
+        <div className="reviewsContainer">
+            <ReviewForm tripID={params.id} updateReviews={updateReviews} />
+            {reviews.map(review => {
+                return (
+                    <Reviews 
+                        key={review.id}
+                        review={review}
+                        user={user}
+                        onUpdateReview={handleUpdateReview}
+                        onDeleteReview={handleDeleteReview}
+                    />
+                )
+            })}
+        </div>
     );
 }
-
-
-
-
-
-
-
-
-
-
 
 export default ReviewsContainer;
