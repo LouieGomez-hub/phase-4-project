a3a732ad2348f0f5073b42de5842dc8060aa552b
louie_gomez <kinggomez027.lg@gmail.com>
2023-04-10 12:01:32 -0400

    added dependent destroy to relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
     validates :username, presence: true, uniqueness: true
     validates :password, confirmation: true
 
-    has_many :trips
-    has_many :reviews
+    has_many :trips, dependent: :destroy
+    has_many :reviews, dependent: :destroy
     has_many :trips, through: :reviews
 end
