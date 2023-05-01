2275597df4056a793b5bc96d7e70ec9a3cfe4253
louie_gomez <kinggomez027.lg@gmail.com>
2023-05-01 12:01:39 -0400

    added alt to img tag

--- client/src/components/TripDetails.js
+++ client/src/components/TripDetails.js
@@ -8,7 +8,7 @@ function TripDetails({ trip }) {
         <div>
             <Link className="details" to={`/trips/${id}`}/>
             <div className="trip-cards">
-                <img src={image_url} />
+                <img src={image_url} alt=""/>
                 <article className="card-details">
                     <div>
                         <h2>{name}</h2>
